de3ebc3c1521ee368ef95a8668e00dc048721f83
unknown <Administrator@FREESKYC-E2D835.(none)>
2014-02-18 14:20:10 +0800

    first commit

---
+++ config.inc.php
@@ -0,0 +1,11 @@
+<?php 
+return array(
+	'DB_TYPE'			=>	'mysql',
+	'DB_HOST'			=>	'localhost',
+	'DB_NAME'			=>	'rubydb',//��Ҫ�½�һ����ݿ⣡���ֽ�
+	'DB_USER'			=>	'root',		//��ݿ��û���	
+	'DB_PWD'			=>	'123456',//��ݿ��¼����
+	'DB_PORT'			=>	'3306',
+	'DB_PREFIX'		=>	'think_',//��ݿ����ǰ׺
+);
+?>
